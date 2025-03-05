b2909076383f3069ac91a98759ee0f5493f7045f
Pallette03 <paul.boebinger@gmail.com>
2025-03-05 22:40:41 +0100

    accuracy calculation for validation

--- Scripts/KeypointDetector.py
+++ Scripts/KeypointDetector.py
@@ -1,7 +1,8 @@
 import datetime
 import os
-import cv2
+import sys
 import numpy as np
+import scipy
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -9,9 +10,9 @@ from torch.utils.data import DataLoader
 from LegoKeypointDataset import LegoKeypointDataset
 import torchvision.transforms as transforms
 import time
-import torch.nn.functional as F
-import matplotlib.pyplot as plt
 from unet_model import UNet
+import wandb
+import threading
 
 
 class FocalLoss(nn.Module):
@@ -120,9 +121,72 @@ def collate_fn(batch):
 
     return {"image": images, "norm_corners": corners_list}
 
+def wait_for_termination(run_handler):
+    while True:
+        if input("Press 'q' to quit: ") == 'q':
+            run_handler.finish()
+            break
+
+def calculate_accuracy(pred_keypoints, target_keypoints, distance_threshold=10, global_image_size=(500, 500)):
+    # Compare batchsize number of predicted keypoints to target keypoints
+    if len(pred_keypoints) != len(target_keypoints):
+        raise ValueError("Batch sizes of predicted and target keypoints do not match")
+    
+    denormalized_target_keypoints = []
+    target_kp_amount = 0
+    for batch_keypoints in target_keypoints:
+        batch_keypoints = [kp * global_image_size[0] for kp in batch_keypoints]
+        denormalized_target_keypoints.append(batch_keypoints)
+        target_kp_amount += len(batch_keypoints)
+    
+    pred_target_distances = []
+    for i in range(len(pred_keypoints)):
+        for pred_point in pred_keypoints[i]:
+            # Find the closest target point
+            closest_target_point = None
+            closest_distance = float("inf")
+            for target_point in denormalized_target_keypoints[i]:
+                distance = np.linalg.norm(pred_point - target_point)
+                if distance < closest_distance:
+                    closest_distance = distance
+                    closest_target_point = target_point
+
+            pred_target_distances.append(closest_distance)
+
+    if len(pred_target_distances) == 0:
+        print("No keypoints found. Very bad.")
+        return -1, -1, 0
+
+    # Calculate the average distance between the predicted and target points
+    total_distance = 0
+    correct_points = 0
+    for distance in pred_target_distances:
+        total_distance += distance
+        if distance < distance_threshold:
+            correct_points += 1 
+
+    return (total_distance / len(pred_target_distances)), (correct_points / len(pred_target_distances)), (correct_points / target_kp_amount)
+        
+
+
+def get_keypoints_from_predictions(pred_heatmaps, threshold=0.5):
+    all_keypoints = []
+    for pred_heatmap in pred_heatmaps:
+        prob_heatmap = torch.sigmoid(pred_heatmap.clone()).squeeze().numpy()
+
+        local_max = scipy.ndimage.maximum_filter(prob_heatmap, size=5)  # Adjust size
+        peaks = (prob_heatmap == local_max) & (prob_heatmap > threshold)
+
+        # Get peak coordinates
+        y_coords, x_coords = np.where(peaks)
+        keypoints = np.column_stack((x_coords, y_coords))
+
+        all_keypoints.append(keypoints)
+
+    return all_keypoints
 
 # Training Loop
-def train_model(model, dataloader, epoch_model_path, num_epochs=5, lr=1e-3, global_image_size=(500, 500), gaussian_blur=False):
+def train_model(model, dataloader, epoch_model_path, num_epochs=5, lr=1e-3, global_image_size=(500, 500), gaussian_blur=False, run_handler=None, termination_thread=None, validataion_params=None):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Training on {device}")
     model = model.to(device)
@@ -139,6 +203,9 @@ def train_model(model, dataloader, epoch_model_path, num_epochs=5, lr=1e-3, glob
         epoch_start_time = time.time()
 
         for batch in dataloader:
+            if termination_thread.is_alive() == False:
+                sys.exit(0)
+
             images = batch["image"].to(device)  # Shape: [batch_size, 3, H, W]
             target_corners = batch["norm_corners"].to(device)  # Shape: [batch_size, num_corners_in_batch, 2]
 
@@ -162,16 +229,16 @@ def train_model(model, dataloader, epoch_model_path, num_epochs=5, lr=1e-3, glob
             # Check the progress through the batch and print every 5 percent
             time_since_start = time.time() - epoch_start_time
             remaining_time = (time_since_start / (counter / len(dataloader))) - time_since_start
-
+            
             if counter % (len(dataloader) // 20) == 0:
-                print(f"[{datetime.datetime.now()}] At Batch {counter}/{len(dataloader)} for Epoch {epoch + 1} taking {time.time() - batch_start_time:.2f} seconds since last checkpoint. Last Loss: {last_loss:.4f}. Progress: {counter / len(dataloader) * 100:.2f}%. Approx. Time left: {remaining_time:.2f} seconds")
+                log_string = f"[{datetime.datetime.now()}] At Batch {counter}/{len(dataloader)} for Epoch {epoch + 1} taking {time.time() - batch_start_time:.2f} seconds since last checkpoint. Last Loss: {last_loss:.4f}. Progress: {counter / len(dataloader) * 100:.2f}%. Approx. Time left: {remaining_time:.2f} seconds"
+                run_handler.log({"Batch Progress": f"{counter}/{len(dataloader)}", "Time since last checkpoint": (time.time() - batch_start_time), "Last Loss": last_loss, "Progress": (counter / len(dataloader) * 100), "Approx. Time left": remaining_time})
+                print(log_string)
                 batch_start_time = time.time()
 
 
         #Clear vram memory before validation
         torch.cuda.empty_cache()
-
-        #validate_model(model, val_dataloader, global_image_size, critereon=critereon)
 
         # Log epoch stats
         print(f"Took {time.time() - start_time:.2f} seconds for epoch {epoch + 1}")
@@ -182,47 +249,93 @@ def train_model(model, dataloader, epoch_model_path, num_epochs=5, lr=1e-3, glob
         print(f"Saving model to {epoch_model_path}")
         torch.save(model.state_dict(), epoch_model_path)
         print(f"Saved model to {epoch_model_path}")
+
+        if validataion_params:
+            validate_model(model, **validataion_params)
+
     return model
 
-def validate_model(model, dataloader, global_image_size, gaussian_blur=False):
+def validate_model(model, dataloader, global_image_size, gaussian_blur=False, threshold=0.5, distance_threshold=10, termination_thread=None, run_handler=None):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Validating on {device}")
-    model.eval()
-    model = model.to(device)
-    total_corner_loss = 0.0
-    critereon = CombinedLoss(lambda_bce=1.0, lambda_mse=0.1, lambda_focal=1.0, alpha=0.25, gamma=2.0)
-    val_counter = 0
-    val_batch_start_time = time.time()
+    torch.cuda.empty_cache()
 
-    for batch in dataloader:
-        images = batch["image"].to(device)  # Shape: [batch_size, 3, H, W]
-        target_corners = batch["norm_corners"].to(device)  # Shape: [batch_size, 1, H, W]
-        #target_offsets = batch["offsets"].to(device)  # Shape: [batch_size, 2, H, W]
-        #mask = batch["mask"].to(device)  # Shape: [batch_size, 1, H, W], 1 for valid corner locations
-        predicted_corners = model(images)#, predicted_offsets
-        
-        # Use normal MSE loss for now but ignore 0
-        corner_loss = corner_loss = heatmap_loss(predicted_corners, target_corners, image_size=global_image_size[0], device=device, critereon=critereon, gaussian_blur=gaussian_blur)
-        val_last_loss = corner_loss.item()
-        
-        # Accumulate losses for logging
-        total_corner_loss += corner_loss.item()
+    with torch.no_grad():
+        model = model.to(device)
+        total_corner_loss = 0.0
+        critereon = CombinedLoss(lambda_bce=1.0, lambda_mse=0.1, lambda_focal=1.0, alpha=0.25, gamma=2.0)
+        val_counter = 0
+        val_batch_start_time = time.time()
+        average_distance = 0
+        accuracy = 0
+        recall = 0
+        no_points_detected = 0
 
-        val_counter += 1
-        # Check the progress through the batch and print every 5 percent
-        if (val_counter % (len(dataloader) // 20) == 0) or (val_counter == len(dataloader)):
-            print(f"[{datetime.datetime.now()}] At Batch {val_counter}/{len(dataloader)} for Validation taking {time.time() - val_batch_start_time:.2f} seconds since last checkpoint. Last Loss: {val_last_loss:.4f}")
-            val_batch_start_time = time.time()
+        for batch in dataloader:
+            if termination_thread.is_alive() == False:
+                sys.exit(0)
+            
+            images = batch["image"].to(device)  # Shape: [batch_size, 3, H, W]
+            target_corners = batch["norm_corners"].to(device)  # Shape: [batch_size, 1, H, W]
+            #target_offsets = batch["offsets"].to(device)  # Shape: [batch_size, 2, H, W]
+            #mask = batch["mask"].to(device)  # Shape: [batch_size, 1, H, W], 1 for valid corner locations
+            predicted_corners = model(images)#, predicted_offsets
+            
+            # Use normal MSE loss for now but ignore 0
+            corner_loss = corner_loss = heatmap_loss(predicted_corners, target_corners, image_size=global_image_size[0], device=device, critereon=critereon, gaussian_blur=gaussian_blur)
+            val_last_loss = corner_loss.item()
+            
+            # Accumulate losses for logging
+            total_corner_loss += corner_loss.item()
+
+            batch_average_distance, batch_accuracy, batch_recall = calculate_accuracy(get_keypoints_from_predictions(predicted_corners.detach().cpu(), threshold=threshold), target_corners.detach().cpu().numpy(), distance_threshold, global_image_size)
+            if batch_average_distance == -1:
+                no_points_detected += 1
+            else:
+                average_distance += batch_average_distance
+                accuracy += batch_accuracy
+                recall += batch_recall
+
+            val_counter += 1
+            # Check the progress through the batch and print every 5 percent
+            if (val_counter % (len(dataloader) // 20) == 0) or (val_counter == len(dataloader)):
+                print(f"[{datetime.datetime.now()}] At Batch {val_counter}/{len(dataloader)} for Validation taking {time.time() - val_batch_start_time:.2f} seconds since last checkpoint. Last Loss: {val_last_loss:.4f}")
+                val_batch_start_time = time.time()
+        
+        average_distance /= (len(dataloader) - no_points_detected)
+        accuracy /= (len(dataloader) - no_points_detected)
+        recall /= (len(dataloader))
+        run_handler.log({"Validation Loss": total_corner_loss / len(dataloader), "Validation Average Distance": average_distance, "Validation Accuracy": accuracy, "Recall": recall})
+        print(f"Validation Loss: {total_corner_loss / len(dataloader):.4f}")
     
-    print(f"Validation Loss: {total_corner_loss / len(dataloader):.4f}")
-    
+    torch.cuda.empty_cache()
     return total_corner_loss / len(dataloader)
 
 if __name__ == "__main__":
 
+    run = wandb.init(
+        project='lego-keypoint-detection', 
+        entity='pallette-personal', 
+        job_type='train',
+        config={
+            "model": "UNet",
+            "dataset": "cropped_objects",
+            "batch_size": 6,
+            "val_batch_size": 6,
+            "learning_rate": 1e-4,
+            "global_image_size": (650, 650),
+            "num_epochs": 10,
+            "gaussian_blur": True,
+            "post_processing_threshold": 0.5,
+            "distance_threshold": 10
+            }
+        )
+
     #Clear vram memory
     torch.cuda.empty_cache()
 
+    termination_thread = threading.Thread(target=wait_for_termination, args=(run,), daemon=True)
+    termination_thread.start()
 
     # Paths
     model_path = os.path.join(os.path.dirname(__file__), os.pardir, 'output', 'dynamic_corner_detector.pth')
@@ -230,15 +343,18 @@ if __name__ == "__main__":
     train_dir = os.path.join(os.path.dirname(__file__), os.pardir, 'datasets', 'cropped_objects', 'train')
     validate_dir = os.path.join(os.path.dirname(__file__), os.pardir, 'datasets', 'cropped_objects', 'validate')
 
-    only_validate = False
-    gaussian_blur = True
+    with_validate = True
 
     print(f"Paths: {model_path}, {epoch_model_path}, {train_dir}, {validate_dir}")
 
-    batch_size = 6
-    val_batch_size = 2
-    global_image_size = (650, 650)
-    learning_rate = 1e-4
+    batch_size = run.config["batch_size"]
+    val_batch_size = run.config["val_batch_size"]
+    global_image_size = run.config["global_image_size"]
+    learning_rate = run.config["learning_rate"]
+    num_epochs = run.config["num_epochs"]
+    gaussian_blur = run.config["gaussian_blur"]
+    threshold = run.config["post_processing_threshold"]
+    distance_threshold = run.config["distance_threshold"]
 
     transform_1 = transforms.Compose([
         transforms.Resize(global_image_size),
@@ -253,25 +369,40 @@ if __name__ == "__main__":
 
     # Train the model
     
-    if not only_validate:
-        # Dataset and DataLoader
-        print("Loading training dataset...")
-        train_dataset = LegoKeypointDataset(os.path.join(train_dir, 'annotations'), os.path.join(train_dir, 'images'), transform=transform_1)
-        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+    
+    # Dataset and DataLoader
+    print("Loading training dataset...")
+    train_dataset = LegoKeypointDataset(os.path.join(train_dir, 'annotations'), os.path.join(train_dir, 'images'), transform=transform_1)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
 
-        print("Training the model...")
-        model = train_model(model, train_dataloader, epoch_model_path, num_epochs=5, lr=learning_rate, global_image_size=global_image_size, gaussian_blur=gaussian_blur)
-    else:
+    if with_validate:
         print("Loading validation dataset...")
         val_dataset = LegoKeypointDataset(os.path.join(validate_dir, 'annotations'), os.path.join(validate_dir, 'images'), transform=transform_1)
         val_dataloader = DataLoader(val_dataset, batch_size=val_batch_size, shuffle=False, collate_fn=collate_fn)
-
+        
+        validataion_params = {
+            "dataloader": val_dataloader,
+            "global_image_size": global_image_size,
+            "gaussian_blur": gaussian_blur,
+            "threshold": threshold,
+            "distance_threshold": distance_threshold,
+            "termination_thread": termination_thread,
+            "run_handler": run
+        }
+        
         print("Validating the model...")
         model.load_state_dict(torch.load(epoch_model_path))
-        validate_model(model, val_dataloader, global_image_size, gaussian_blur=gaussian_blur)
+        validate_model(model, **validataion_params)
+        # print("Training the model...")
+        # model = train_model(model, train_dataloader, epoch_model_path, num_epochs=num_epochs, lr=learning_rate, global_image_size=global_image_size, gaussian_blur=gaussian_blur, run_handler=run, termination_thread=termination_thread, validataion_params=validataion_params)
+    
+    else:
+        print("Training the model...")
+        model = train_model(model, train_dataloader, epoch_model_path, num_epochs=num_epochs, lr=learning_rate, global_image_size=global_image_size, gaussian_blur=gaussian_blur, run_handler=run, termination_thread=termination_thread, validataion_params=None)
 
     # Save the model
     print("Saving the model...")
     torch.save(model.state_dict(), model_path)
+    run.finish()
 
         
